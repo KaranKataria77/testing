3d7901326198506d8649e118bad767d673ff2137
Karan.Kataria <karankataria771801@gmail.com>
2022-10-04 12:33:09 +0530

    added

---
+++ src/components/ElectionHomeWIdget.js
@@ -0,0 +1,8 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable eqeqeq */
+/* eslint-disable jsx-a11y/alt-text */
+/* eslint-disable react/jsx-no-target-blank */
+/* eslint-disable no-script-url */
+/* eslint-disable jsx-a11y/anchor-is-valid */
+/* eslint-disable no-undef */
+
